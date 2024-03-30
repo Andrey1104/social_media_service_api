af51c43ccd73f558fab6b918a8ff6fdc7801055b
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-03-30 17:42:33 +0200

    added admin in social_media app

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_media.models import Post, Comment, Like, Follower, Event, Message
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Follower)
+admin.site.register(Event)
+admin.site.register(Message)
