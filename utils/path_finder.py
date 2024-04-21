67a8ac4019187a20720caaffe881976799f3f7bd
Andrii Malovanyi <malovaniy.an@gmail.com>
2024-04-21 19:26:27 +0300

    fix: update path_finder logic

--- utils/path_finder.py
+++ utils/path_finder.py
@@ -1,11 +1,17 @@
 import os
 import uuid
 
+from django.contrib.auth import get_user_model
 from django.utils.text import slugify
 
 
 def image_file_path(instance, filename):
+    catalog = "posts"
+    _attr = instance.title
+    if isinstance(instance, get_user_model()):
+        catalog = "avatars"
+        _attr = instance.email
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.email)}-{uuid.uuid4()}{extension}"
+    filename = f"{slugify(_attr)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/avatars/", filename)
+    return os.path.join("uploads", catalog, filename)
